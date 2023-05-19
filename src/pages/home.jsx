cc3d86d297f566867afe472e25486cbb890e9fa5
Nathan Maciel <nathannmvr@gmail.com>
2023-05-18 21:34:57 -0300

    Criado 3 telas

---
+++ src/pages/home.jsx
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+
+export default function Home() {
+    return(
+        <View className="flex-1 items-center bg-gray-800">
+            <Text className="color-white mt-[100px] text-xl font-bold">Turmas</Text>
+            <Text className="color-gray-400 mt-[5px]">Jogue com a sua turma</Text>
+            <ScrollView className="flex-1 mt-[30px]">
+                <TouchableOpacity activeOpacity={1} className="rounded-md bg-gray-700 h-[85px] w-[310px] flex-row items-center justify-center">
+                    <Text className="color-gray-200 text-[20px]">Nome da turma</Text>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={1} className="rounded-md bg-gray-700 mt-[10px] h-[85px] w-[310px] flex-row items-center justify-center">
+                    <Text className="color-gray-200 text-[20px]">Nome da turma</Text>
+                </TouchableOpacity>
+            </ScrollView>
+            <TouchableOpacity activeOpacity={1} className="rounded-md bg-green-700 h-[50px] w-[310px] flex-row items-center justify-center mb-[20px]">
+                <Text className="color-white text-[20px]">Criar nova turma</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
---
+++ src/pages/turma.jsx
@@ -0,0 +1,53 @@
+import { View, Text, TextInput, TouchableOpacity, Image, ScrollView } from "react-native";
+
+export default function Turma() {
+    return (
+        <View className="flex-1 items-center bg-gray-800">
+            <View className="mt-[30px] w-[310]">
+                <TouchableOpacity>
+                    <Image source={require('../img/voltar.png')} className="h-[40px] w-[40px]"></Image>
+                </TouchableOpacity>
+            </View>
+            <Text className="color-white text-xl font-bold mt-[30px]">Nome da Turma</Text>
+            <Text className="color-gray-400 mt-[5px]">Adicione a galera e Separe os times</Text>
+            <View className="flex-row mt-7">
+                <TextInput className="items-start justify-start bg-gray-900 text-gray-400 w-[260px] rounded-l-md pl-4" placeholder="Nome do participante" placeholderTextColor={"#9ca3af"}  />
+                <TouchableOpacity activeOpacity={1} className="bg-gray-900 flex-row items-center justify-center h-[50px] w-[50px] rounded-r-md">
+                    <Text className="color-green-700 text-[32px]">+</Text>
+                </TouchableOpacity>
+            </View>
+            <View className="flex-row w-[310px] mt-6">
+                <View className="flex-row flex-1">
+                    <TouchableOpacity className="mr-2 border-2 p-2 border-green-500 rounded-sm">
+                        <Text className="color-white font-bold">TIME A</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity className="mr-2 border-2 p-2 border-gray-800">
+                        <Text className="color-gray-200 font-bold">TIME B</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text className="border-2 p-2 border-gray-800 text-gray-200 font-bold">2</Text>
+            </View>
+            <ScrollView>
+                <View className="flex-row mt-7">
+                    <View className="items-center justify-center bg-gray-700 text-gray-400 text-center w-[260px] rounded-l-md">
+                        <Text className="color-gray-200">Rodrigo Gonçalves</Text>
+                    </View>
+                    <TouchableOpacity activeOpacity={1} className="bg-gray-700 flex-row items-center justify-center h-[50px] w-[50px] rounded-r-md">
+                        <Text className="color-red-800 text-[20px]">X</Text>
+                    </TouchableOpacity>
+                </View>
+                <View className="flex-row mt-3">
+                    <View className="items-center justify-center bg-gray-700 text-gray-400 text-center w-[260px] rounded-l-md">
+                        <Text className="color-gray-200">Rodrigo Gonçalves</Text>
+                    </View>
+                    <TouchableOpacity activeOpacity={1} className="bg-gray-700 flex-row items-center justify-center h-[50px] w-[50px] rounded-r-md">
+                        <Text className="color-red-800 text-[20px]">X</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+            <TouchableOpacity activeOpacity={1} className="rounded-md bg-red-500 h-[50px] w-[310px] flex-row items-center justify-center mb-3">
+                <Text className="color-white font-bold">Remover turma</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
